383eda5cbd76b17e8dc33c2c28ac36d8e8af8fa3
chris110408 <leichen@LeideMacBook-Pro-2.local>
2016-08-23 15:49:43 -0700

    add image scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :images
   root 'welcome#index'
   devise_for :users, :controller => {:registrations => 'registrations'}
   # The priority is based upon order of creation: first created -> highest priority.
